771c2485cbd33fcba04e8f0c4ca3d3377e929cba
shjang7 <lucy.sh.jang@gmail.com>
2019-10-31 19:27:02 +0900

    Add exception error case for calculator

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,7 +1,31 @@
 const calculator = {
-	Add : function(x, y) {
-		return x + y;
-	},
-}
+  validCheck(a, b) {
+    if (a.constructor !== Number || b.constructor !== Number) {
+      throw new TypeError('Invalid input');
+    }
+  },
+  zeroDivisionCheck(_, divider) {
+    if (!divider || divider === 0) {
+      throw new Error('Zero division');
+    }
+  },
+  add(x, y = 0) {
+    this.validCheck(x, y);
+    return x + y;
+  },
+  subtract(x, y = 0) {
+    this.validCheck(x, y);
+    return x - y;
+  },
+  multiply(x, y = 1) {
+    this.validCheck(x, y);
+    return x * y;
+  },
+  divide(x, y = 1) {
+    this.validCheck(x, y);
+    this.zeroDivisionCheck(x, y);
+    return Math.round((x / y) * 100) / 100;
+  },
+};
 
 module.exports = calculator;
--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -1,9 +1,72 @@
 const calculator = require('./calculator');
 
-test('add two positive numbers', () => {
-	expect(2+2).toBe(4)
-});
+describe('calculator', () => {
+  describe('add', () => {
+    test('two numbers', () => {
+      expect(calculator.add(1, 2)).toBe(3);
+    });
+    test('a number returns itself', () => {
+      expect(calculator.add(1)).toBe(1);
+    });
+    test('three numbers calculates only first two numbers', () => {
+      expect(calculator.add(1, 2, 5)).toBe(3);
+    });
+    test('non numbers error with array', () => {
+      expect(() => calculator.add([], [])).toThrow(TypeError);
+    });
+    test('non numbers error with string', () => {
+      expect(() => calculator.add('a', 'b')).toThrow(TypeError);
+    });
+  });
 
-test('add two negative numbers', () => {
-	expect(3+3).toBe(6)
+  describe('subtract', () => {
+    test('second number from first number', () => {
+      expect(calculator.subtract(2, 1)).toBe(1);
+    });
+    test('a number returns itself', () => {
+      expect(calculator.subtract(2)).toBe(2);
+    });
+    test('three numbers calculates only first two numbers', () => {
+      expect(calculator.subtract(2, 1, 5)).toBe(1);
+    });
+    test('non numbers error', () => {
+      expect(() => calculator.subtract('a', 'b')).toThrow(TypeError);
+    });
+  });
+
+  describe('multiply', () => {
+    test('two numbers', () => {
+      expect(calculator.multiply(2, 4)).toBe(8);
+    });
+    test('a number returns itself', () => {
+      expect(calculator.multiply(2)).toBe(2);
+    });
+    test('three numbers calculates only first two numbers', () => {
+      expect(calculator.multiply(2, 4, 6)).toBe(8);
+    });
+    test('non numbers error', () => {
+      expect(() => calculator.multiply('a', 'b')).toThrow(TypeError);
+    });
+  });
+
+  describe('divide', () => {
+    test('by second number for first number', () => {
+      expect(calculator.divide(10, 5)).toBe(2);
+    });
+    test('returns rounded at 2 decimal point', () => {
+      expect(calculator.divide(10, 3)).toBe(3.33);
+    });
+    test('a number returns itself', () => {
+      expect(calculator.divide(10)).toBe(10);
+    });
+    test('three numbers calculates only first two numbers', () => {
+      expect(calculator.divide(10, 5, 3)).toBe(2);
+    });
+    test('zero division error', () => {
+      expect(() => calculator.divide(10, 0)).toThrow(Error);
+    });
+    test('non numbers error', () => {
+      expect(() => calculator.divide('a', 'b')).toThrow(TypeError);
+    });
+  });
 });
